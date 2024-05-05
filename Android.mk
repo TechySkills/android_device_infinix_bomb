55a15bad059cb7e4ded020803780dc664996abdf
TechySkills <82161063+TechySkills@users.noreply.github.com>
2024-05-05 23:48:09 +0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,6 +18,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), X690)
+ifeq ($(TARGET_DEVICE), bomb)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
